b67a0be1d4859dbce47401b223372770f2227a31
Capputino <109418758+Capputino@users.noreply.github.com>
2022-08-06 08:30:51 -0300

    Add files via upload

--- f.js
+++ f.js
@@ -1,11 +1,11 @@
 
 function login() {
-  var Login = document.getElementById('username').value
+  var Login = document.getElementById('logar').value
   var password = document.getElementById('password').value
         
  
 
-     if (login =="admin" && senha=="admin") {
+     if (login =="123" && senha=="123") {
        alert("sucesso") 
        location.href = "home.html"
       }else{
